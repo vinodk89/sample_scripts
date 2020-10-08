ab9481e5da7f080fa382a6ce2c479091c6f6ed8e
vinodk89 <57387854+vinodk89@users.noreply.github.com>
2020-10-08 14:21:53 +0530

    Update test_parsing.pl

--- test_parsing.pl
+++ test_parsing.pl
@@ -9,84 +9,82 @@ use feature 'say';
 
 my %data = (
   '1342' => [
-			  {
-				'Severity' => 'MEDIUM',
-				'Node' => 'Node002',
-				'State' => 'ACTIVE_UNACKNOWLEDGED',
-				'Updated' => '2020-10-01T12:00:00',
-				'eventId' => '1342'
-			  },
-			  {
-				'Severity' => 'HIGH',
-				'Node' => 'Node002',
-				'Updated' => '2020-10-01T12:05:00',
-				'eventId' => '1342',
-				'State' => 'ACTIVE_UNACKNOWLEDGED'
-			  },
-			  {
-				'Severity' => 'CLEARED',
-				'Node' => 'Node002',
-				'Updated' => '2020-10-01T12:10:00',
-				'eventId' => '1342',
-				'State' => 'ACTIVE_UNACKNOWLEDGED'
-			  }
-			],
+		  {
+			'Severity' => 'MEDIUM',
+			'Node' => 'Node002',
+			'State' => 'ACTIVE_UNACKNOWLEDGED',
+			'Updated' => '2020-10-01T12:00:00',
+			'eventId' => '1342'
+		  },
+		  {
+			'Severity' => 'HIGH',
+			'Node' => 'Node002',
+			'Updated' => '2020-10-01T12:05:00',
+			'eventId' => '1342',
+			'State' => 'ACTIVE_UNACKNOWLEDGED'
+		  },
+		  {
+			'Severity' => 'CLEARED',
+			'Node' => 'Node002',
+			'Updated' => '2020-10-01T12:10:00',
+			'eventId' => '1342',
+			'State' => 'ACTIVE_UNACKNOWLEDGED'
+		  }
+		],
   '1341' => [
-			  {
-				'State' => 'ACTIVE_UNACKNOWLEDGED',
-				'eventId' => '1341',
-				'Updated' => '2020-10-01T12:10:00',
-				'Node' => 'Node001',
-				'Severity' => 'HIGH'
-			  },
-			  {
-				'State' => 'ACTIVE_UNACKNOWLEDGED',
-				'Updated' => '2020-10-01T12:15:00',
-				'eventId' => '1341',
-				'Severity' => 'HIGH',
-				'Node' => 'Node001'
-			  },
-			  {
-				'State' => 'CLEARED_ACKNOWLEDGED',
-				'Updated' => '2020-10-01T12:15:00',
-				'eventId' => '1341',
-				'Severity' => 'CLEARED',
-				'Node' => 'Node001'
-			  }
-			]
+		  {
+			'State' => 'ACTIVE_UNACKNOWLEDGED',
+			'eventId' => '1341',
+			'Updated' => '2020-10-01T12:10:00',
+			'Node' => 'Node001',
+			'Severity' => 'HIGH'
+		  },
+		  {
+			'State' => 'ACTIVE_UNACKNOWLEDGED',
+			'Updated' => '2020-10-01T12:15:00',
+			'eventId' => '1341',
+			'Severity' => 'HIGH',
+			'Node' => 'Node001'
+		  },
+		  {
+			'State' => 'CLEARED_ACKNOWLEDGED',
+			'Updated' => '2020-10-01T12:15:00',
+			'eventId' => '1341',
+			'Severity' => 'CLEARED',
+			'Node' => 'Node001'
+		  }
+		]
 );
 
 foreach my $ev (sort keys %data) { 
-	my $ra = $data{$ev}; 
+    my $ra = $data{$ev}; 
 
     # Initialize "best" time and its hashref
     my $hr = $ra->[0]; 
 	
     my $dt = time_ts( $hr->{Updated} );
 
-	foreach my $idx (1..$#$ra) { 
+    foreach my $idx (1..$#$ra) { 
         my $dt_curr = time_ts( $ra->[$idx]{Updated} );
-		
+
         if ($dt_curr > $dt) {
-            $dt = $dt_curr;
-            $hr = $ra->[$idx];
+	    $dt = $dt_curr;
+	    $hr = $ra->[$idx];
         }
         elsif ($dt_curr == $dt) { 
-            if ($ra->[$idx]{Severity} eq 'CLEARED') { 
-                $dt = $dt_curr;
-                $hr = $ra->[$idx];
-			}
-			
-		}
-	}   
-	say "Event: $hr->{eventId}";
+	    if ($ra->[$idx]{Severity} eq 'CLEARED') { 
+	        $dt = $dt_curr;
+	        $hr = $ra->[$idx];
+	    }	
+        }
+    }   
+    say "Event: $hr->{eventId}";
     say "\t", join ',', grep { $_ ne $hr->{eventId} } values %{$hr};
 }
 
 sub time_ts {
 	
-	my $date = shift; 
-	
+    my $date = shift; 
     my ($yyyy, $mm, $dd, $hr, $min, $sec) = ($date =~ /(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)/);
   
     my $epoch_seconds = timelocal($sec, $min, $hr, $dd, $mm, $yyyy);
@@ -101,4 +99,3 @@ Event: 1341
 Event: 1342
         ACTIVE_UNACKNOWLEDGED,2020-10-01T12:10:00,CLEARED,Node002
 =cut
-
